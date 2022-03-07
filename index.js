1fa039845a34e3ccb5ba725db4709635da1bef75
eternal <eternal.git@gmail.com>
2022-03-07 18:32:30 +0000

    go away

--- index.js
+++ index.js
@@ -13,8 +13,4 @@ export default class ForceDarkMode extends Plugin {
    stop() {
       unpatchAll('force-dark-mode');
    }
-
-   getSettingsPanel() {
-      powercord.api.notices.sendToast('test', { icon: { icon: 'info-circle', color: '#737bd7'}, header: 'Custom functionality', content: 'Did you know unbound lets you run custom functionality when the user clicks the settings cog? This allows me to open a toast without changing the page and rendering settings!', timeout: 10000 })
-   }
 }
